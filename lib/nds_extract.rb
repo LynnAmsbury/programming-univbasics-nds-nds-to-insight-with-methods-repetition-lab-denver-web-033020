ef9b0450edcb858685cd6b8ca77baaab4bba6de5
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-13 20:59:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -52,7 +52,7 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  
+
   director_earnings_hash = directors_totals(source)
   director_names = list_of_directors(source)
   index = 0
