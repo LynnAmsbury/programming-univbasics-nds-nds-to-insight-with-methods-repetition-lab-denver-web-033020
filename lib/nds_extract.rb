566eb52bb2af9f4dd20a977d7b0888476586ad0e
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-13 21:07:01 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -55,8 +55,8 @@ def total_gross(source)
 
   director_earnings_hash = directors_totals(source) # The result of directors_totals (saved in its hash)
   director_names_array = list_of_directors(source) # The result of list_of_directors (saved in its array)
-  index = 0 # Counter for total gross earnings
-  total = 0 # Counter for
+  index = 0 # Counter for
+  total = 0 # Counter for total gross earnings
     while index < director_names_array.length do # While the current index is less than the length of the director_names hash do...
       dir_name = director_names_array[index] # Assign the current (index) director's name to dir_name
       total += director_earnings_hash[dir_name] # Assign and increment the total director_earnings_hash
